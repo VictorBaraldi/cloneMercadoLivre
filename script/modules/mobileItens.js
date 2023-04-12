0d34fc45288c8fdeb2fc9f2ec7267946c00986f2
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-04-12 08:30:10 -0300

    Inicio modo mobile

--- script/modules/mobileItens.js
+++ script/modules/mobileItens.js
@@ -1,8 +1,44 @@
 export default class RemoveItem {
-  constructor(element) {
-    this.element = document.querySelector(element);
+  constructor() {
+    this.removeItens = [
+      '.nav-header-disney',
+      '.nav-header-conta',
+      '.nav-itens',
+      '.nav-itens',
+    ];
   }
-  remove() {
-    this.element.remove();
+  remove(item) {
+    const removeItem = document.querySelector(item);
+    removeItem.remove();
+  }
+
+  loopItens(element, func) {
+    if (this.screenX)
+      element.forEach((element) => {
+        func(element);
+      });
+  }
+
+  screenX() {
+    this.screenX = window.screen.width;
+    return this.screenX < 1000 ? true : false;
+  }
+
+  add() {
+    const container = document.querySelector('.nav-grid');
+    const mobile = document.createElement('button');
+    const carrinho = document.createElement('button');
+    const endereco = document.createElement('div');
+    mobile.innerText = 'mobile';
+    carrinho.innerText = 'carrinho';
+    endereco.innerHTML = 'Digite seu endereço';
+    container.appendChild(mobile);
+    container.appendChild(carrinho);
+    container.appendChild(endereco);
+  }
+
+  init() {
+    this.loopItens(this.removeItens, this.remove);
+    this.add();
   }
 }
--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
+import RemoveItem from './modules/mobileItens.js';
 import slideAutoPlay from './modules/slideAutoPlay.js';
 import { slider } from './modules/sliderConfig.js';
 
 slideAutoPlay(slider, '.glider');
+const mobile = new RemoveItem();
+mobile.init();
